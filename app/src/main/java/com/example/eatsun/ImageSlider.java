f9288b696589e35e65de75e63f7ca2a4223c4a6f
5psy <tndudehdrb1@gmail.com>
2022-05-17 15:21:53 +0900

    lmageSlider

---
+++ app/src/main/java/com/example/eatsun/ImageSlider.java
@@ -0,0 +1,49 @@
+package com.example.eatsun;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import androidx.annotation.NonNull;
+import androidx.viewpager.widget.PagerAdapter;
+
+public class ImageSlider extends PagerAdapter {
+
+    private int[] images = {R.drawable.image01, R.drawable.image02, R.drawable.image03, R.drawable.image04, R.drawable.image05,
+            R.drawable.image06,R.drawable.image07, R.drawable.image08, R.drawable.image09, R.drawable.image10, R.drawable.image11};
+    private LayoutInflater inflater;
+    private Context context;
+
+    public ImageSlider(Context context){
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+
+        return images.length;
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+        return view == ((LinearLayout) object);
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position){
+        inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View v = inflater.inflate(R.layout.Slider, container, false);
+        ImageView imageView = (ImageView) v.findViewById(R.id.imageView);
+        imageView.setImageResource(images[position]);
+        container.addView(v);
+        return v;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object){
+        container.invalidate();
+    }
+}
