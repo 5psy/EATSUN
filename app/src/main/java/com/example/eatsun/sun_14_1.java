ee6667e2ba0d6cae96aac3d2b3ed21a7553cd03c
5psy <tndudehdrb1@gmail.com>
2022-05-22 20:48:59 +0900

    mypage connect

--- app/src/main/java/com/example/eatsun/sun_14_1.java
+++ app/src/main/java/com/example/eatsun/sun_14_1.java
@@ -4,7 +4,7 @@ import android.os.Bundle;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class Name_rule extends AppCompatActivity {
+public class sun_14_1 extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
