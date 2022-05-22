fa37c0d89d9b81545c310c150d985432ac0a13ec
kimhy0eun <dhy04029@naver.com>
2022-05-22 21:35:02 +0900

    sun_09_2 connect

---
+++ app/src/main/java/com/example/eatsun/RS_2.java
@@ -0,0 +1,24 @@
+package com.example.eatsun;
+
+import android.content.Intent;
+import android.view.View;
+import android.widget.ImageView;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+public class RS_2 extends AppCompatActivity {
+
+    ImageSlider imageSlider;
+    ViewPager viewPager;
+    private Button butn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sun_09_2);
+    }
+}
---
+++ app/src/main/java/com/example/eatsun/sun_9_2.java
@@ -0,0 +1,4 @@
+package com.example.eatsun;
+
+public class sun_9_2 {
+}
