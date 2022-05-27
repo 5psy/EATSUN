732bd0301382e6f24cecf5c70cd14ef858768296
SUYOUNG-YOO <suyoung_99@naver.com>
2022-05-27 21:25:19 +0900

    button

---
+++ app/src/main/java/com/example/eatsun/CategoryActivity.java
@@ -0,0 +1,56 @@
+package com.example.eatsun;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CategoryActivity extends AppCompatActivity{
+
+    Button button1, button2, button3, button4;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sun_10);
+        Intent intent=getIntent();
+
+        button1=findViewById(R.id.korean_food);
+        button2=findViewById(R.id.bunsik2);
+        button3=findViewById(R.id.chinese_food);
+        button4=findViewById(R.id.western_food);
+
+        button1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(getApplicationContext(), sun_11_1.class);
+                startActivity(intent);
+            }
+        });
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), sun_11_3.class);
+                startActivity(intent);
+            }
+        });
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), sun_11_6.class);
+                startActivity(intent);
+            }
+        });
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), sun_11_5.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
