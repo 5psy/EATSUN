0655a4e583dcc63542338bacb94f732aac80d8e2
5psy <tndudehdrb1@gmail.com>
2022-05-28 03:10:01 +0900

    countdown connect

--- app/src/main/java/com/example/eatsun/sun_14_2.java
+++ app/src/main/java/com/example/eatsun/sun_14_2.java
@@ -2,16 +2,45 @@ package com.example.eatsun;
 
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+
 public class sun_14_2 extends AppCompatActivity {
+    TextView countdown_tv;
+    Button button;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sun_14_2);
+        button = findViewById(R.id.btn);
+        countdown_tv = findViewById(R.id.countdown_tv);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                button.setEnabled(false);
+                new CountDownTimer(2400000, 1000) {
+                    @Override
+                    public void onTick(long millisUntilFinished) {
+                        NumberFormat format = new DecimalFormat("00");
+                        long minute = (millisUntilFinished/60000)%60;
+                        long second = (millisUntilFinished/1000)%60;
+                        countdown_tv.setText(format.format(minute)+":"+format.format(second));
+                    }
+
+                    @Override
+                    public void onFinish() {
+                        countdown_tv.setText("00:00");
+                        button.setEnabled(true);
+                    }
+                }.start();
+            }
+        });
     }
 }
