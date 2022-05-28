06892c83848681ce51ecc51b27fd2e5c3e11d3f0
5psy <tndudehdrb1@gmail.com>
2022-05-29 00:57:48 +0900

    class & button

--- app/src/main/java/com/example/eatsun/sun_11_1.java
+++ app/src/main/java/com/example/eatsun/sun_11_1.java
@@ -1,6 +1,10 @@
 package com.example.eatsun;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,5 +13,14 @@ public class sun_11_1 extends AppCompatActivity {
     protected  void onCreate(@Nullable Bundle saveInstanceState){
         super.onCreate(saveInstanceState);
         setContentView(R.layout.sun_11_1);
+
+        ImageView back6 = (ImageView) findViewById(R.id.back6);
+        back6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), CategoryActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/eatsun/sun_14_2.java
+++ app/src/main/java/com/example/eatsun/sun_14_2.java
@@ -1,96 +1,54 @@
 package com.example.eatsun;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 import java.text.DecimalFormat;
 import java.text.NumberFormat;
 import java.util.Locale;
 
 public class sun_14_2 extends AppCompatActivity {
-    private static final long START_TIME_IN_MILLIS = 2400000;
-    private long mTimeLeftMillis = START_TIME_IN_MILLIS;
-
-    TextView countdown_tv;
-    Button buttonenter;
-    Button buttonexit;
-    CountDownTimer CountDownTimer;
-    boolean TimerRunning;
-
+    Button button;
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sun_14_2);
-        buttonenter = findViewById(R.id.enter);
-        buttonexit = findViewById(R.id.exit);
-        countdown_tv = findViewById(R.id.countdown_tv);
 
-        buttonenter.setOnClickListener(new View.OnClickListener() {
+
+        button = findViewById(R.id.enter);
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startTimer();
-               /* button.setEnabled(false);
-                new CountDownTimer(2400000, 1000) {
-                    @Override
-                    public void onTick(long millisUntilFinished) {
-                        NumberFormat format = new DecimalFormat("00");
-                        long minute = (millisUntilFinished/60000)%60;
-                        long second = (millisUntilFinished/1000)%60;
-                        countdown_tv.setText(format.format(minute)+":"+format.format(second));
-                    }
-
-                    @Override
-                    public void onFinish() {
-                        countdown_tv.setText("00:00");
-                        button.setEnabled(true);
-                    }
-                }.start();*/
+                Intent intent = new Intent(sun_14_2.this,countdown.class);
+                startActivity(intent);
             }
         });
-
-        buttonexit.setOnClickListener(new View.OnClickListener() {
+        ImageView back13 = (ImageView) findViewById(R.id.back13);
+        back13.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                resetTimer();
+                Intent intent = new Intent(getApplicationContext(), mainScreen.class);
+                startActivity(intent);
             }
         });
-    }
-
-    private void startTimer(){
-        CountDownTimer = new CountDownTimer(mTimeLeftMillis, 1000) {
+/*        button = findViewById(R.id.exit);
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onTick(long millisUntilFinished) {
-                mTimeLeftMillis = millisUntilFinished;
-                updateCountDownText();
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), mainScreen.class);
+                startActivity(intent);
             }
-
-            @Override
-            public void onFinish() {
-                countdown_tv.setText("00:00");
-            }
-        }.start();
-
-        TimerRunning = false;
-    }
-    private void resetTimer(){
-        CountDownTimer.cancel();
-        TimerRunning = true;
-        mTimeLeftMillis = START_TIME_IN_MILLIS;
-        updateCountDownText();
-    }
-    private void updateCountDownText(){
-        int min = (int) (mTimeLeftMillis / 1000) / 60;
-        int sec = (int) (mTimeLeftMillis / 1000) % 60;
-
-        String timeLeftFormatted = String.format(Locale.getDefault(),"%02d:%02d", min, sec);
-
-        countdown_tv.setText(timeLeftFormatted);
+        });*/
     }
 }
