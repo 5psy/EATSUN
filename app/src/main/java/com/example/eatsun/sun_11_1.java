6e222f5bf1b6835d94426da8842735d5212cd70b
SUYOUNG-YOO <suyoung_99@naver.com>
2022-05-29 01:14:44 +0900

    BUTTON

--- app/src/main/java/com/example/eatsun/sun_11_1.java
+++ app/src/main/java/com/example/eatsun/sun_11_1.java
@@ -1,6 +1,10 @@
 package com.example.eatsun;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.content.Intent;
+
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,5 +13,22 @@ public class sun_11_1 extends AppCompatActivity {
     protected  void onCreate(@Nullable Bundle saveInstanceState){
         super.onCreate(saveInstanceState);
         setContentView(R.layout.sun_11_1);
+
+        ImageView actionModeCloseDrawable = (ImageView) findViewById(R.id.back6);
+        actionModeCloseDrawable.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),CategoryActivity.class);
+                startActivity(intent);
+            }
+        });
+        ImageView ic_media_ff = (ImageView) findViewById(R.id.next);
+        ic_media_ff.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), sun_11_2.class);
+                startActivity(intent);
+            }
+        });
     }
 }
