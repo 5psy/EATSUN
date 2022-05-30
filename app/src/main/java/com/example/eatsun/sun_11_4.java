5c39186a2edfcf52be49955f9761461741a277a6
SUYOUNG-YOO <suyoung_99@naver.com>
2022-05-30 09:48:47 +0900

    sun_11_3_order

---
+++ app/src/main/java/com/example/eatsun/sun_11_4.java
@@ -0,0 +1,13 @@
+package com.example.eatsun;
+
+import android.os.Bundle;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class sun_11_4 extends AppCompatActivity {
+    @Override
+    protected  void onCreate(@Nullable Bundle saveInstanceState) {
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.sun_11_4);
+    }
+}
