28a154b4b6b738f4db0fd7e5b439324f9053140a
SUYOUNG-YOO <suyoung_99@naver.com>
2022-06-03 16:27:10 +0900

    payment

---
+++ app/src/main/java/com/example/eatsun/payment.java
@@ -0,0 +1,27 @@
+package com.example.eatsun;
+
+import android.media.Image;
+import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class payment extends AppCompatActivity {
+
+    @Override
+    protected  void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.payment);
+
+        ImageView btn_dialog =  (ImageView) findViewById(R.id.cancel);
+        btn_dialog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), ReservationSeat.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
